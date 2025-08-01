1cea38ed3ad15c7d7ab8d8ad6958fae5e636095b
Khaled <khaled.abdelrahman2023@gmail.com>
2025-08-01 10:06:31 +0300

    allow front-end to talk to backend. using cors

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
